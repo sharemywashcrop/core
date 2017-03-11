b4c23e5e5a1b76eeff453c20df50c86d13636d7e
Thomas Michelot <thomas.michelot@epita.fr>
2017-03-11 21:43:51 +0000

    db

---
+++ db.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 11 21:17:42 2017
+
+@author: Thomas
+"""
+
+import sqlite3
+
+class db:
+    
+    connection = None
+    
+    def __init__(self):
+        self.connection = pymysql.connect (host = "198.100.148.10",
+                              user = "sharemywash",
+                              passwd = "epitaepita",
+                              db = "sharemywash")
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
