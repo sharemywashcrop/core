59be06e44fe65d8802b08d9119c6a190d83be3ba
Thomas Michelot <thomas.michelot@epita.fr>
2017-03-12 00:09:59 +0000

    why not

--- user.py
+++ user.py
@@ -13,5 +13,9 @@ class user:
         self.name = n
         self.email = e
         self.token = t
-        self.location ="" # evol("(233,234)")
+        self.location = l # evol("(233,234)")
+        self.getloc(l) #put the actual tupple in it
         self.pseudo = p
+        
+    def getloc(self,loc):
+        self.location = eval(loc);
